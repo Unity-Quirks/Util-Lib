6cacd6add291e8525694913bf0f25498e16453f7
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-12-07 18:51:35 +0100

    Updated dotNet-Quirks

--- Runtime/Quirks/Enumq.cs
+++ Runtime/Quirks/Enumq.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Quirks
 {
@@ -28,11 +30,35 @@ namespace Quirks
             return setBits;
         }
 
+        /// <summary>Gets all enum values as any array.</summary>
+        public static T[] GetAllValues<T>() => (T[])Enum.GetValues(typeof(T));
+
+        /// <summary>Gets a random enum value.</summary>
+        /// <typeparam name="T">The enum type</typeparam>
+        /// <returns>A randomly selected enum value.</returns>
         public static T GetRandomEnum<T>()
         {
             Array values = Enum.GetValues(typeof(T));
             return (T)values.GetValue(randomGen.Next(values.Length));
         }
+
+        /// <summary>Gets a random enum value, excluding specified values.</summary>
+        /// <typeparam name="T">The enum type</typeparam>
+        /// <param name="excludedValues">Array of enum values to exclude from random selection.</param>
+        /// <returns>A randomly selected enum value.</returns>
+        public static T GetRandomEnumExcluded<T>(params T[] excludedValues)
+        {
+            Array values = Enum.GetValues(typeof(T));
+            Array filteredValues = values.Cast<T>().Where(value => !excludedValues.Contains(value)).ToArray();
+
+            return (T)filteredValues.GetValue(randomGen.Next(filteredValues.Length));
+        }
+
+        /// <summary>Gets a random enum value, excluding specified values.</summary>
+        /// <typeparam name="T">The enum type</typeparam>
+        /// <param name="excludedValues">IEnumerable of enum values to exclude from random selection.</param>
+        /// <returns>A randomly selected enum value.</returns>
+        public static T GetRandomEnumExcluded<T>(IEnumerable<T> excludedValues) => GetRandomEnumExcluded(excludedValues.ToArray());
 
         /// <summary>Returns the highest numeric value for T.</summary>
         public static int GetHighestValue<T>()
