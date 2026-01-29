f33f0e20c3be0ffad24cd8ba6ae03e56d2df92d0
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-01-29 20:47:01 +0100

    Update TransformExtensions.cs

--- Runtime/Extensions/TransformExtensions.cs
+++ Runtime/Extensions/TransformExtensions.cs
@@ -5,6 +5,8 @@ namespace Quirks
 {
     public static class TransformExtensions
     {
+        #region Set X Y Z
+
         public static void SetX(this Transform transform, float value)
         {
             transform.position = new Vector3(value, transform.position.y, transform.position.z);
@@ -18,11 +20,41 @@ namespace Quirks
             transform.position = new Vector3(transform.position.x, transform.position.y, value);
         }
 
+        public static void SetLocalX(this Transform transform, float value)
+        {
+            transform.localPosition = new Vector3(value, transform.localPosition.y, transform.localPosition.z);
+        }
+        public static void SetLocalY(this Transform transform, float value)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, value, transform.localPosition.z);
+        }
+        public static void SetLocalZ(this Transform transform, float value)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, value);
+        }
+
+        #endregion
+
         public static void Reset(this Transform transform)
         {
             transform.position = Vector3.zero;
             transform.rotation = Quaternion.identity;
             transform.localScale = Vector3.one;
+        }
+
+        public static void ResetLocal(this Transform transform)
+        {
+            transform.localScale = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+        }
+
+        #region Children
+
+        public static void SetActiveChildren(this Transform transform, bool active)
+        {
+            foreach (Transform child in transform)
+                child.gameObject.SetActive(active);
         }
 
         public static Transform[] GetChildren(this Transform transform)
@@ -35,6 +67,7 @@ namespace Quirks
 
             return children;
         }
+
         public static List<Transform> GetDescendants(this Transform transform)
         {
             List<Transform> descendants = new List<Transform>();
@@ -47,6 +80,25 @@ namespace Quirks
             return descendants;
         }
 
+        public static void DetachChildren(this Transform transform)
+        {
+            while(transform.childCount > 0)
+            {
+                transform.GetChild(0).SetParent(null);
+            }
+        }
+
+        public static Transform FindChildWithTag(this Transform transform, string tag)
+        {
+            foreach(Transform child in transform)
+            {
+                if (child.CompareTag(tag))
+                    return child;
+            }
+
+            return null;
+        }
+
         public static void Clear(this Transform transform)
         {
             foreach (Transform child in transform)
@@ -55,6 +107,18 @@ namespace Quirks
             }
         }
 
+        public static void ClearImmediate(this Transform transform)
+        {
+            while(transform.childCount > 0)
+            {
+                GameObject.DestroyImmediate(transform.GetChild(0).gameObject);
+            }
+        }
+
+        #endregion
+
+        #region Translation
+
         public static void LerpTowards(this Transform transform, Vector3 targetPosition, float speed)
         {
             transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
@@ -62,6 +126,24 @@ namespace Quirks
         public static void LerpTowards(this Transform transform, Transform targetPosition, float speed)
         {
             LerpTowards(transform, targetPosition.position, speed);
+        }
+
+        #endregion
+
+
+
+        public static void SetLayerRecursively(this Transform transform, int layer)
+        {
+            transform.gameObject.layer = layer;
+            foreach (Transform child in transform)
+                child.SetLayerRecursively(layer);
+        }
+
+        public static void SetLayerRecursively(this Transform transform, string layerName)
+        {
+            int layer = LayerMask.NameToLayer(layerName);
+            if (layer != -1)
+                transform.SetLayerRecursively(layer);
         }
     }
 }
